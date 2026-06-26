69ae8aa6cb84b7ff79ac22a9cd275c326b3dff78
Ronaldo S. <ronaldosm276@gmail.com>
2026-06-26 19:03:28 -0300

    finalizado contas básicas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 
 const valorMetro = 550
 const dadosForm = document.querySelector('#formulario')
-const lote = document.querySelector('#lote')
 const quadra = document.querySelector('#quadra')
+const lote = document.querySelector('#lote')
 const comprimento = document.querySelector('#comprimento')
 const largura = document.querySelector('#largura')
+const resultado = document.querySelector('#info')
 
 
 dadosForm.addEventListener('submit', (evt) => {
     evt.preventDefault()
-   
+   //ainda não sei o que isso faz
+
+
     let m2 = (comprimento.value * largura.value)
     let valorTotal = (valorMetro * m2)
     
@@ -20,5 +23,14 @@ dadosForm.addEventListener('submit', (evt) => {
     console.log(quadra.value)
     console.log(comprimento.value)
     console.log(largura.value)
+    
+    resultado.innerHTML += `Quadra ${quadra.value} - Lote ${lote.value} Comprimento = ${comprimento.value} Largura = ${largura.value} Valor total = ${valorTotal} <br>`
 
+    lote.value = ''
+    quadra.value = ''
+    comprimento.value = ''
+    largura.value = ''
 })
+
+
+//no mínimo criar um código básico, depois ver se consegue aplicar arrow function além do addEventListener
