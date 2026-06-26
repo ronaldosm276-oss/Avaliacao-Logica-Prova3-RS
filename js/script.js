0dc9a4e14c4ec593fd4a7509e73adc8dc4ce2c67
Ronaldo S. <ronaldosm276@gmail.com>
2026-06-26 19:22:53 -0300

    backup

--- js/script.js
+++ js/script.js
@@ -10,21 +10,58 @@ const resultado = document.querySelector('#info')
 
 dadosForm.addEventListener('submit', (evt) => {
     evt.preventDefault()
+
    //ainda não sei o que isso faz
 
 
     let m2 = (comprimento.value * largura.value)
     let valorTotal = (valorMetro * m2)
     
+    /*
+    Valor do lote                     Valor adicional
+
+    Até R$ 20.000,00                       Isento
+
+    Entre R$ 20.000,00 até R$ 100.000,00    5%
+
+    Entre R$ 100.000,00 até R$ 500.000,00   10%
+
+    Entre R$ 500.000,00 até R$1.000.000,00  15%
+
+    Acima de R$ 1.000.000,00                20%
+    */
   
+    let valorAdd = 0
+
+    if (valorTotal <= 20000){
+        valorAdd = 'Isento'
+    }
+    else if(valorTotal > 20000 && valorTotal <= 100000){
+        valorAdd = 'R$' + valorTotal * 0.05
+    }
+    else if(valorTotal > 100000 && valorTotal <= 500000){
+        valorAdd = 'R$' + valorTotal * 0.10
+    }
+    else if(valorTotal > 500000 && valorTotal <= 1000000){
+        valorAdd = 'R$' + valorTotal * 0.15
+    }
+    else if(valorTotal > 1000000){
+        valorAdd =  'Aplicado 20%, logo : R$' + valorTotal * 0.20
+    }
+    else{
+        valorAdd = '???'
+    }
+
+    resultado.innerHTML += `Quadra ${quadra.value} - Lote ${lote.value} Comprimento = ${comprimento.value} Largura = ${largura.value} Valor total = R$${valorTotal} reais Valor Adicional: ${valorAdd} reais <br>`
+
     console.log(m2)
     console.log(valorTotal)
     console.log(lote.value)
     console.log(quadra.value)
     console.log(comprimento.value)
     console.log(largura.value)
+    console.log((valorAdd))
     
-    resultado.innerHTML += `Quadra ${quadra.value} - Lote ${lote.value} Comprimento = ${comprimento.value} Largura = ${largura.value} Valor total = ${valorTotal} <br>`
 
     lote.value = ''
     quadra.value = ''
